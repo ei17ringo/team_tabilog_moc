d008c9eee1b2d73d7880b5ecfb2f608697a1afbd
M-Sh1bata <shibata.taro.cebu@gmail.com>
2017-01-30 22:14:59 +0800

    google mapのマーカーとマーカークリック時の動作を作成

--- Top_template/custom/js/tabilog.js
+++ Top_template/custom/js/tabilog.js
@@ -1,11 +1,39 @@
 // google map
 function initMap() {
+
+
   var opts = {
     zoom: 15,
-    center: new google.maps.LatLng(35.6807527,139.7670716)
+    center: new google.maps.LatLng(-37.81319, 144.96298)
   };
   var map = new google.maps.Map(document.getElementById("map"), opts);
+
+
+
+             // 情報ウィンドウ
+			// var infowindow1 = new google.maps.InfoWindow({
+			// 	position:map.getCenter(), 
+   //              map:map,
+			//   content: '場所：'+'メルボルン'+"<br />"+'おすすめ度：'+'★★★☆☆',
+			// });
+
+
+			  // マーカー
+             var marker = new google.maps.Marker({ 
+                position:map.getCenter(), 
+                map:map,
+            }); 
+
+            　//マーカークリック時に動作する情報ウィンドウ 
+            google.maps.event.addListener(marker, "click", function() {
+			var infowindow = new google.maps.InfoWindow({
+			content: '場所：'+'メルボルン'+"<br />"+'おすすめ度：'+'★★★☆☆'
+			});
+			infowindow.open(map, marker);
+			});
+
 }
+
 
 
 // carousel
